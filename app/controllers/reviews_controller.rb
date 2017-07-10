e034a1742a4a1e2e8dc7fbb9268c43e7b3cb3a43
Kai deRochemont and Kat Gifford <kat@>
2017-07-10 10:07:40 -0700

    add CRUD functionality to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,46 @@
 class ReviewsController < ApplicationController
 
   def new
-
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
   end
 
   def create
-
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review successfully added"
+      redirect_to product_path(@product)
+    else
+      flash[:notice] = "Review creation failed"
+      render :new
+    end
   end
 
   def edit
-
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
   end
 
   def update
-
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "Review successfully updated"
+      redirect_to product_path(@product)
+    else
+      flash[:notice] = "Review update failed"
+      render :edit
+    end
   end
 
   def destroy
-
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    if @review.destroy
+      flash[:notice] = "Review successfully removed"
+      redirect_to product_path(@product)
+    end
   end
 
 private
