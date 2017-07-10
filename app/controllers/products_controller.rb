68f510cc4c4c4675c1a668a0f52e1a2db93ba05c
Kai deRochemont and Kat Gifford <kat@>
2017-07-10 10:02:41 -0700

    add CRUD functionality to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,23 +9,41 @@ class ProductsController < ApplicationController
   end
 
   def new
-
+    @product = Product.new
   end
 
   def create
-
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully added"
+      redirect_to products_path
+    else
+      render :new
+      flash[:notice] = "Product creation failed"
+    end
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def update
-
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product successfully updated"
+      redirect_to product_path(@product)
+    else
+      flash[:notice] = "Product update failed"
+      render :edit
+    end
   end
 
   def destroy
-
+    @product = Product.find(params[:id])
+    if @product.destroy
+      flash[:notice] = "Product successfully removed"
+      redirect_to products_path
+    end
   end
 
 private
