3d8ce92bb5d7284db7a43da9f0054913228e750a
Kai deRochemont and Kat Gifford <kat@>
2017-07-10 16:47:57 -0700

    add calculate-total

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,26 @@
 class Order < ApplicationRecord
+  before_save :update_total
+  before_create :update_status
+
   has_many :products, through: :order_items
   has_many :order_items
   # validates :status, presence: true
   # validates :total, numericality: true, presence: true
+
+  def calculate_total
+    self.order_items.collect { |item| item.product.price * item.quantity }.sum
+  end
+
+private
+
+  def update_total
+      self.total = calculate_total
+  end
+
+  def update_status
+    if self.status == nil?
+      self.status = "in progress"
+    end
+  end
+
 end
