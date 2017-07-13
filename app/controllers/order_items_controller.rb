1009a85a3cfb9a1668e5c9251b715ba981281b10
Kai Derochemont and Kat Gifford <kai@>
2017-07-13 10:44:24 -0700

    add method to consolidate cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,19 +4,22 @@ class OrderItemsController < ApplicationController
   def create
     @account = Account.where(user_id: current_user.id)[0]
     @order = current_order
-    @item = @order.order_items.new(item_params)
     @order.account_id = @account.id
-    if @order.save
+    item_id = params[:order_item][:product_id].to_i
+    item_quantity = params[:order_item][:quantity].to_i
+    if cart_duplicate(current_order, item_id)
+      consolidate_items(current_order, item_id, item_quantity)
+      @order.save
+    else
+      @item = @order.order_items.new(item_params)
+      @order.save
+    end
       flash[:alert] = "Item added to cart"
       session[:order_id] = @order.id
       respond_to do |format|
         format.html { redirect_to products_path }
         format.js
       end
-    else
-      flash[:notice] = "Error adding item to cart"
-      redirect_to products_path
-    end
   end
 
   def destroy
@@ -31,6 +34,16 @@ private
 
   def item_params
     params.require(:order_item).permit(:quantity, :product_id)
+  end
+
+  def cart_duplicate(order, item_id)
+    order.order_items.any? { |item| item.product_id == item_id }
+  end
+
+  def consolidate_items(order, item_id, item_quantity)
+    item = order.order_items.where(product_id: item_id)[0]
+    new_quantity = item_quantity + item.quantity
+    item.update(quantity: new_quantity)
   end
 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,14 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   validates :quantity, presence: true, :numericality => { :greater_than_or_equal_to => 1 }
 
+  # def cart_duplicate(order)
+  #   order.order_items.any? { |item| item.product_id == self.product.id }
+  # end
+  #
+  # def consolidate_items(order)
+  #   item = order.order_items.where(product_id: self.product_id)[0]
+  #   new_quantity = self.quantity + item.quantity
+  #   item.update(quantity: new_quantity)
+  # end
+
 end
